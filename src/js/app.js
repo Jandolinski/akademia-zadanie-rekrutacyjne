a71e14198aefda3f2a91181f0761f7cd81653ed2
Jan Doliński <jan.dolinski89@gmail.com>
2020-10-06 20:58:30 +0200

    Add JS for last section

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,8 @@ const changeSection = direction => {
 
     const currentSection = document.querySelector('.active[data-step]');
 
+    const sectionsCount = Array.from(document.querySelectorAll('[data-step]')).length;
+
     if(!currentSection) {
 
         return false;
@@ -15,6 +17,7 @@ const changeSection = direction => {
 
     if(direction === 'next') {
 
+        // Switch from first section to multiStepSection
         if(currentSectionNumber == 1) {
 
             document.getElementById('multiStepSection').classList.remove('hidden');
@@ -34,6 +37,7 @@ const changeSection = direction => {
 
     } else if(direction === 'prev') {
 
+        // Switch from multiStepSection to first section
         if(currentSectionNumber == 2) {
 
             document.getElementById('multiStepSection').classList.add('hidden');
@@ -53,13 +57,27 @@ const changeSection = direction => {
 
     }
 
+    // Hide sections navigation on last step
+
+    const sectionsNavigation = document.getElementById('sectionsNavigation');
+
+    if(currentSectionNumber == sectionsCount) {
+
+        sectionsNavigation.classList.add('hidden');
+
+    } else {
+
+        sectionsNavigation.classList.remove('hidden');
+
+    }
+
     refreshTimeline(currentSectionNumber);
 
 }
 
 const refreshTimeline = currentSection => {
 
-    const timelineItems = document.querySelectorAll('.timeline__item[data-timeline-step]');
+    const timelineItems = Array.from(document.querySelectorAll('.timeline__item[data-timeline-step]'));
 
     timelineItems.forEach((el) => {
 
@@ -84,10 +102,45 @@ const refreshTimeline = currentSection => {
 
 }
 
+const refreshInputs = () => {
+
+    const inputs = Array.from(document.querySelectorAll('input[type="checkbox"], input[type="radio"]'));
+
+    inputs.forEach(el => {
+
+        el.checked = false;
+
+    });
+
+}
+
+const refreshCalculator = () => {
+    
+    const currentSection = document.querySelector('.active[data-step]');
+    const firstSection = document.querySelector('[data-step="1"]');
+    const multiStepSection = document.getElementById('multiStepSection');
+    
+    refreshInputs();
+
+    refreshTimeline(1);
+
+    multiStepSection.classList.add('hidden');
+
+    currentSection.classList.add('hidden');
+    currentSection.classList.remove('active');
+
+    firstSection.classList.remove('hidden');
+    firstSection.classList.add('active');
+
+
+}
+
 const startCalculator = document.getElementById('start');
 const nextBtn = document.getElementById('nextSection');
 const prevBtn = document.getElementById('prevSection');
+const restart = document.getElementById('restart');
 
 startCalculator.addEventListener('click', () => changeSection('next'));
 nextBtn.addEventListener('click', () => changeSection('next'));
 prevBtn.addEventListener('click', () => changeSection('prev'));
+restart.addEventListener('click', refreshCalculator);
